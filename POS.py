6b2feb0c97faed7ee4f7e8a4d2f77d241e41be15
eltonmar <elton.marinho97@gmail.com>
2024-09-27 18:24:17 -0300

    comitando pra testar

--- POS.py
+++ POS.py
@@ -2,13 +2,13 @@ import requests
 
 url = "https://api.userede.com.br/redelabs/merchant-statement/v2/sales"
 
-companyNumbers = [86819747]
+companyNumbers = [84417072]
 subsidiaries = companyNumbers
 
-startdate = "2024-09-25"
-enddate = "2024-09-25"
+startdate = "2024-09-10"
+enddate = "2024-09-10"
 
-access_token = "eyJraWQiOiI4ZDk0OTg1Ny00MTVmLTRiODEtYjZmNC05OTZhNDY0ODUzMDciLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3JsNy1wcmQtYXBpLnVzZXJlZGVjbG91ZC5jb20uYnIvb2F1dGgvdjIiLCJpYXQiOjE3MjczODU2ODQsIm5iZiI6MTcyNzM4NTY4NCwiYXVkIjoiaHR0cHM6Ly9ybDctcHJkLWFwaS51c2VyZWRlY2xvdWQuY29tLmJyL29hdXRoL3YyIiwiZXhwIjoxNzI3Mzg3MTI0LCJhcHAiOiIzMzA1MDI0NjAwMDEyN18xMjczN18wMSIsInZlciI6IjEuMCIsIm9yZyI6ImM4ZmIxYTZhLTA4ZTMtNDVlMC05NWExLTg3NDViYmI3ZWI4YyIsInNjb3BlIjoibWVyY2hhbnQtc3RhdGVtZW50IGZlYXR1cmVfbWVyY2hhbnRfc3RhdGVtZW50IiwiY2VsbCI6IjAiLCJjaG5sIjoiMCIsImNpZCI6IjdiNzliMjY1LTYxYzItNGJiYi04ZTZiLWRhNjQzYzA5YjE4YiIsInVzaWQiOiIxMjczNzQ0Zi0xYWYyLTQyZTYtODEyMi00MjIyZDBlMjlkZDEifQ.A5t0WgTvRcOsDzc0El6AnPtg6KddbSlI8TZFqGZ-4QfLdyCJK3C2q4Tsi8ZORlRb7tYlbpJKTw0w5NCwPrLciVJpggh5l2bifiAaxqH-XHbM1Yw1LnpiHJPR6x0e48gLhlNeI3Ant1lncUdBSp2LhE2RP5kPonzpP_1Qj2MUIxwE8ybF2PnXBJ-0W70C8Fy6UbEIizcD9bE0t6g8TnKxgorf-PDtUAFsMh25f0use5feVQ6SacvMrk4uoUcPwqcwJ15fL1k9T3C0bywMpDIK5lcH9CCp0ESWUxSGgFCwJKnfXYClqnNfjlonmaeBehnE6tllZ_x0TiYraaehsA_lNQ"
+access_token = "eyJraWQiOiI4ZDk0OTg1Ny00MTVmLTRiODEtYjZmNC05OTZhNDY0ODUzMDciLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3JsNy1wcmQtYXBpLnVzZXJlZGVjbG91ZC5jb20uYnIvb2F1dGgvdjIiLCJpYXQiOjE3Mjc0NjkxMjMsIm5iZiI6MTcyNzQ2OTEyMywiYXVkIjoiaHR0cHM6Ly9ybDctcHJkLWFwaS51c2VyZWRlY2xvdWQuY29tLmJyL29hdXRoL3YyIiwiZXhwIjoxNzI3NDcwNTYzLCJhcHAiOiIzMzA1MDI0NjAwMDEyN18xMjczN18wMSIsInZlciI6IjEuMCIsIm9yZyI6ImM4ZmIxYTZhLTA4ZTMtNDVlMC05NWExLTg3NDViYmI3ZWI4YyIsInNjb3BlIjoibWVyY2hhbnQtc3RhdGVtZW50IGZlYXR1cmVfbWVyY2hhbnRfc3RhdGVtZW50IiwiY2VsbCI6IjAiLCJjaG5sIjoiMCIsImNpZCI6IjdiNzliMjY1LTYxYzItNGJiYi04ZTZiLWRhNjQzYzA5YjE4YiIsInVzaWQiOiIxMjczNzQ0Zi0xYWYyLTQyZTYtODEyMi00MjIyZDBlMjlkZDEifQ.gNqNWVxEEFxUD_qphLK-1E1PJoDfORdruShJ2J7yIWNYTxV6b9AwQXQ8s6lSmFDKeSnG858srSF0_N2x9cqfqqkizqOnnmL7mT0tuEdCzvXE9MmKHLxrxPlDqNiaXUxNSxzTFikVoKknSFOG0zGrzCw5RDxKapuMgeEhETnDQmbybvAnWBOVZancxslNnmvifEtb-KdI2hzm24dWLNGIclBLTQgLqvqA7Y_vnp_3LZJ7OBAWwsEkQK-1FuwetONVFi7fyNDkLmZ1l5UxXbWStDPX793YjgNYkbIwDq6tlE4g1rdS99hzSOXEaJZ5-0LCf-yHrzAxdIxpJ52NoBTu4w"
 
 headers = {
     "Content-Type": "application/json",
@@ -20,33 +20,45 @@ params = {
     "endDate": enddate,
     "parentCompanyNumber": companyNumbers,
     "subsidiaries": subsidiaries,
-    "page": 1,
     "size": 100
 }
 
 all_transactions = []
+nextKey = ""
+
 
 while True:
+
+    if nextKey:
+        params["pageKey"] = nextKey
+    else:
+        params.pop("pageKey", None)
+
     response = requests.get(url, params=params, headers=headers)
 
     if response.status_code == 200:
-
         dados = response.json()
-
+        print(dados)
         if 'content' in dados and 'transactions' in dados['content']:
             transactions = dados['content']['transactions']
             all_transactions.extend(transactions)
 
-            if len(transactions) < params["size"]:
-                break
+            print(f"Transações recebidas nesta página: {len(transactions)}")
+
+            if 'nextKey' in dados:
+                nextKey = dados['nextKey']
+                print(f"Próxima chave de paginação encontrada: {nextKey}")
             else:
-                params["page"] += 1
+                print("Chave 'nextKey' não foi encontrada. Finalizando iteração.")
+                break
+
         else:
             print("A chave 'transactions' não foi encontrada dentro de 'content'.")
             break
     else:
         print(f"Erro na requisição para {companyNumbers}: {response.status_code}")
         break
+
 
 for transaction in all_transactions:
     filtered_data = {
@@ -76,4 +88,7 @@ for transaction in all_transactions:
            'type': transaction.get('modality', {}).get('type', 'N/A'),
         }
     }
-    print(filtered_data)
+
+
+print(f"Total de transações: {len(all_transactions)}")
+
